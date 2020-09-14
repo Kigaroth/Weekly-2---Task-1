ad811da76b323d7a08aedbb40e9af7417b2d7266
Kigaroth <hp_20@hotmail.com>
2020-09-14 18:22:53 +0200

    Add project files.

---
+++ Weekly 2 - Task 1/Weekly 2 - Task 1.cpp
@@ -0,0 +1,17 @@
+/*
+*Weekly 2 - Task 1.cpp
+    Write a program that writes numbers from 1 to 100
+    1. Use a while-loop
+    2. Use a for-loop
+*/
+
+
+
+int main()
+{
+    int i = 0;
+    while (i < 100) {
+        std::cout << i << endl; i++;
+    }
+}
+
